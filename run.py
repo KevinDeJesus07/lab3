616d2fe89cb963680b2cb4acc13b50360a46cc98
Kevin <kincer@uninorte.edu.co>
2025-11-20 14:57:26 -0500

    fix: localhost

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ app = create_app()
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    host = '0.0.0.0' if os.environ.get('RENDER') or os.environ.get('PORT') else 'localhost'
+    host = '0.0.0.0' if os.environ.get('RENDER') or os.environ.get('PORT') else '127.0.0.1'
     socketio.run(app, debug=False, host=host, allow_unsafe_werkzeug=True, port=port)
